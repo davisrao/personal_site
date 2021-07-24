ea00acace81ec5909b022c9f0bb647deda793cf4
Davis Rao <davisrao1@gmail.com>
2021-07-24 16:33:58 -0400

    completed everything
    added footer
    fixed Js scrolling to allow them to scroll (removed href"#"),

--- myscript.js
+++ myscript.js
@@ -1,26 +1,28 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    // your code here
 
 
 //calling the function on scroll of window
 window.onscroll = function() {scrollFunction()};
 
-//define my button
+//define top button
 
-let myButton = document.querySelector('#back-to-top');
+let topButton = document.querySelector('#back-to-top');
 
 //details of scrollFunction
 function scrollFunction () {
 //if the distance from top of doc is over 15px, show the button. if not, dont.
     if(document.body.scrollTop > 15 || document.documentElement.scrollTop > 15){
-        myButton.style.display = "block";
+        topButton.style.display = "block";
     }
     else{
-        myButton.style.display = "none";
+        topButton.style.display = "none";
     }
 }
 
 //what happens when we click the button
-myButton.onclick = function(){
+topButton.onclick = function(){
     scrollToTop();
 }
 
@@ -28,3 +30,51 @@ function scrollToTop(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// define the about button & where to scroll
+let aboutButton = document.querySelector('#aboutButton');
+let aboutSection = document.querySelector('#aboutSection')
+
+//call the function on click of aboutButton
+
+aboutButton.onclick = scrollToAbout
+
+//deatils of scrollToAbout;
+
+function scrollToAbout(){
+    aboutSection.scrollIntoView({ behavior: 'smooth'});
+};
+}, false);
+
+
+// define the contact button & where to scroll
+let contactButton = document.querySelector('#contactButton');
+let contactSection = document.querySelector('#contactSection')
+
+//call the function on click of Contact
+
+contactButton.onclick = scrollToContact
+
+//deatils of scrollToContact;
+
+function scrollToContact(){
+    contactSection.scrollIntoView({ behavior: 'smooth'});
+};
+
+
+
+
+
+// define the fun button & where to scroll
+let funButton = document.querySelector('#funButton');
+let funSection = document.querySelector('#funSection')
+
+//call the function on click of fun
+
+funButton.onclick = scrollToFun
+
+//deatils of scrollToFun;
+
+function scrollToFun(){
+    funSection.scrollIntoView({ behavior: 'smooth'});
+};
